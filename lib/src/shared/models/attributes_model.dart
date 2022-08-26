ad6fb0a4d1a7267b0ea13c3a36082be433635ac3
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-08-25 22:19:43 -0300

    feat: add attributes enum on AttributeModel

--- lib/src/shared/models/attributes_model.dart
+++ lib/src/shared/models/attributes_model.dart
@@ -1,9 +1,31 @@
+import 'dart:convert';
+
+import 'package:ordem_paranormal_ficha/src/shared/enums/attributes.dart';
+
 class AttributesModel {
-  final String name;
+  final Attributes attribute;
   final int amount;
 
   AttributesModel({
-    required this.name,
+    required this.attribute,
     required this.amount
   });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'attribute': attribute.toString(),
+      'amount': amount,
+    };
+  }
+
+  factory AttributesModel.fromMap(Map<String, dynamic> map) {
+    return AttributesModel(
+      attribute: Attributes.fromMap(map['attribute'] as String),
+      amount: map['amount'] as int,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory AttributesModel.fromJson(String source) => AttributesModel.fromMap(json.decode(source) as Map<String, dynamic>);
 }
