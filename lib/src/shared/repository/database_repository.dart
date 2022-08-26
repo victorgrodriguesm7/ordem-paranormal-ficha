f6f3c4f1891bb1bfa69301c8a19e7927f9ef21b1
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-08-26 13:37:49 -0300

    feat: create DatabaseRepository

---
+++ lib/src/shared/repository/database_repository.dart
@@ -0,0 +1,40 @@
+import 'package:ordem_paranormal_ficha/src/shared/models/character_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const String characterKey = 'character';
+
+class DatabaseRepository {
+  SharedPreferences? sharedPreferences;
+  bool loading = true;
+
+  DatabaseRepository(){
+    _init();
+  }
+
+  _init() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    loading = false;
+  }
+
+  CharacterModel? getCharacterModel() {
+    if (sharedPreferences == null) return null;
+
+    String? rawCharacter = sharedPreferences!.getString(characterKey);
+
+    if (rawCharacter == null) return null;
+
+    return CharacterModel.fromJson(rawCharacter);
+  }
+
+  Future<bool> saveCharacterModel(CharacterModel character) async {
+    if (sharedPreferences == null) return false;
+
+    try {
+      await sharedPreferences!.setString(characterKey, character.toJson());
+
+      return true;
+    } catch(err){
+      return false;
+    }
+  }
+}
