8e1c76e2ed849ec113c1778a3833169502774d7e
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-08-26 13:39:13 -0300

    feat: add DatabaseRepository and AppController as singleton

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -1,9 +1,14 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:ordem_paranormal_ficha/src/app_controller.dart';
 import 'package:ordem_paranormal_ficha/src/modules/modules.dart';
+import 'package:ordem_paranormal_ficha/src/shared/repository/database_repository.dart';
 
 class AppModule extends Module {
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds => [
+    Bind.singleton((i) => DatabaseRepository()),
+    Bind.singleton((i) => AppController(database: i()))
+  ];
 
   @override
   List<ModularRoute> get routes => [
