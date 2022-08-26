2adf8596bdcdabdeb505f5acc8189db283503555
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-08-26 13:36:24 -0300

    feat: create AppStates

---
+++ lib/src/app_state.dart
@@ -0,0 +1,25 @@
+import 'package:ordem_paranormal_ficha/src/shared/models/character_model.dart';
+
+abstract class AppState {}
+
+class InitialAppState extends AppState {
+  final bool Function() loadCharacter;
+
+  InitialAppState({
+    required this.loadCharacter
+  });
+}
+
+class LoadingAppState extends AppState {}
+
+class SuccessAppState extends AppState {
+  final CharacterModel? character;
+  final bool hasCharacterAlready;
+  final Future<bool> Function() saveCharacter;
+  
+  SuccessAppState({
+    required this.character,
+    required this.hasCharacterAlready,
+    required this.saveCharacter
+  });
+}
