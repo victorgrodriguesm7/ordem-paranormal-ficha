1aff6e255b895705e68cd901edd6d661e47e4e99
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-08-26 11:40:41 -0300

    feat: add json serialization on CharacterModel

--- lib/src/shared/models/character_model.dart
+++ lib/src/shared/models/character_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:ordem_paranormal_ficha/src/shared/models/ability_model.dart';
 import 'package:ordem_paranormal_ficha/src/shared/models/annotation_model.dart';
 import 'package:ordem_paranormal_ficha/src/shared/models/attack_model.dart';
@@ -58,4 +60,46 @@ class CharacterModel {
       annotations: []
     );
   }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'life': life,
+      'maxLife': maxLife,
+      'sanity': sanity,
+      'maxSanity': maxSanity,
+      'effortPoints': effortPoints,
+      'maxEffortPoints': maxEffortPoints,
+      'attributes': attributes.map((x) => x.toMap()).toList(),
+      'attacks': attacks.map((x) => x.toMap()).toList(),
+      'defense': defense.toMap(),
+      'rituals': rituals.map((x) => x.toMap()).toList(),
+      'abilities': abilities.map((x) => x.toMap()).toList(),
+      'skills': skills.map((x) => x.toMap()).toList(),
+      'iventory': iventory.map((x) => x.toMap()).toList(),
+      'annotations': annotations.map((x) => x.toMap()).toList(),
+    };
+  }
+
+  factory CharacterModel.fromMap(Map<String, dynamic> map) {
+    return CharacterModel(
+      life: map['life'] as int,
+      maxLife: map['maxLife'] as int,
+      sanity: map['sanity'] as int,
+      maxSanity: map['maxSanity'] as int,
+      effortPoints: map['effortPoints'] as int,
+      maxEffortPoints: map['maxEffortPoints'] as int,
+      attributes: List<AttributesModel>.from((map['attributes'] as List<int>).map<AttributesModel>((x) => AttributesModel.fromMap(x as Map<String,dynamic>),),),
+      attacks: List<AttackModel>.from((map['attacks'] as List<int>).map<AttackModel>((x) => AttackModel.fromMap(x as Map<String,dynamic>),),),
+      defense: DefenseModel.fromMap(map['defense'] as Map<String,dynamic>),
+      rituals: List<RitualModel>.from((map['rituals'] as List<int>).map<RitualModel>((x) => RitualModel.fromMap(x as Map<String,dynamic>),),),
+      abilities: List<AbilityModel>.from((map['abilities'] as List<int>).map<AbilityModel>((x) => AbilityModel.fromMap(x as Map<String,dynamic>),),),
+      skills: List<SkillModel>.from((map['skills'] as List<int>).map<SkillModel>((x) => SkillModel.fromMap(x as Map<String,dynamic>),),),
+      iventory: List<InventoryItemModel>.from((map['iventory'] as List<int>).map<InventoryItemModel>((x) => InventoryItemModel.fromMap(x as Map<String,dynamic>),),),
+      annotations: List<AnnotationModel>.from((map['annotations'] as List<int>).map<AnnotationModel>((x) => AnnotationModel.fromMap(x as Map<String,dynamic>),),),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory CharacterModel.fromJson(String source) => CharacterModel.fromMap(json.decode(source) as Map<String, dynamic>);
 }
