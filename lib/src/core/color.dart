f4e0fbe15b551ad6d058567f18a45af49e836fba
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-08-27 10:54:26 -0300

    feat: create class with all colors

---
+++ lib/src/core/color.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color black = const Color(0xFF0A0909);
+  static Color gray = const Color(0xFF121212);
+  static Color overlay = const Color(0xFF0A0909).withOpacity(0.5);
+  static Color white = const Color(0xFFFFFFFF);
+  static Color darkWhite = const Color(0xFFD9D9D9);
+  static Map<int, Color> red = {
+    100: const Color(0xFF541010),
+    200: const Color(0xFF7A1717),
+    300: const Color(0xFF811313),
+    400: const Color(0xFF941C1C),
+    500: const Color(0xFFA11F1F),
+    600: const Color(0xFFE02B2B)
+  };
+}
