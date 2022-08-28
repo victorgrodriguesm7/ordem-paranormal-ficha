1e0d2641ab24b70d74e1178f3e69b63f82094df1
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-08-28 08:51:17 -0300

    feat: create extensions

---
+++ lib/src/shared/extensions/list.dart
@@ -0,0 +1,8 @@
+extension FirstWhereOrNullExtension<E> on List<E> {
+  E? firstWhereOrNull(bool Function(E) test) {
+    for (E element in this) {
+      if (test(element)) return element;
+    }
+    return null;
+  }
+}
